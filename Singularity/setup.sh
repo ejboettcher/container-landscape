2057f84c176c82556b473269873ccd89274a31b3
Matt Kijowski <mkijowski@users.noreply.github.com>
2017-04-09 18:51:24 -0400

    Update setup.sh

--- Singularity/setup.sh
+++ Singularity/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-git clone git@github.com:singularityware/singularity.git
-cd singularity
-sudo ./configure && make && make install
+VERSION=2.2.1
+wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
+tar xvf singularity-$VERSION.tar.gz
+cd singularity-$VERSION
+./configure --prefix=/usr/local
+make
+sudo make install
