d0da01d8ecf6cd313558ed04ab3d3b85cf838bc6
MatthewKijowski <matthewkijowski@gmail.com>
2017-04-10 09:47:42 -0400

    added standard files

--- Singularity/Apache_Mesos/setup.sh
+++ Singularity/Apache_Mesos/setup.sh
@@ -9,3 +9,5 @@ sudo apt-get install -y openjdk-7-jdk
 #Mesos dependencies
 sudo apt-get -y install build-essential python-dev python-virtualenv libcurl4-nss-dev libsasl2-dev libsasl2-modules maven libapr1-dev libsvn-dev zlib1g-dev
 
+clone from the following and compile from source
+https://git-wip-us.apache.org/repos/asf?p=mesos.git
